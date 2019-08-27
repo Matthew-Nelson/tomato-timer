a0dcf4b23ec5461b2cf9d83b6b2229d6f8761847
Matt <matthewnelson18@gmail.com>
2019-08-26 19:15:04 -0700

    removed material-ui

--- src/Person.js
+++ src/Person.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
 
 export default class Person extends Component {
 
     render() {
         return (
-            <Button variant="contained" color="primary">
-                Hi
-            </Button>
+            <div>we out here</div>
         );
     }
 }
