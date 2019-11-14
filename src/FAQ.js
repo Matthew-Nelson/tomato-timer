9d18ea5ef52e9afb8e35e8d92e94c11c763ef132
Matt <matthewnelson18@gmail.com>
2019-11-13 16:27:15 -0800

    removed 'my site'  links

--- src/FAQ.js
+++ src/FAQ.js
@@ -10,90 +10,185 @@ import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaLaptopCode } from 'react-icons/fa';
 
-
 export default class FAQ extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            modalOpen: false
-        }
-    }
-
-    handleModalOpen = () => {
-        this.setState({
-            modalOpen: true
-        })
-    }
-
-    handleModalClose = () => {
-        this.setState({
-            modalOpen: false
-        })
+    this.state = {
+      modalOpen: false
     };
+  }
 
-    render() {
-        return (
-            <div>
-                <Button variant="contained" color="default" onClick={this.handleModalOpen}>More Info</Button>
-                <Modal open={this.state.modalOpen} onClose={this.handleModalClose} >
-                    <Fade in={this.state.modalOpen} timeout={ {enter: 500, exit: 500} }>
-                        <div className={'modal-window'}>
-                            <div className={"modal-inner-wrapper"}>
-                                <Fab className="modal-close" onClick={this.handleModalClose}>
-                                    <AddIcon />
-                                </Fab>
-                                
-                                <h2>Instructions and More Information</h2>
+  handleModalOpen = () => {
+    this.setState({
+      modalOpen: true
+    });
+  };
 
-                                <p>The Pomodoro Technique is a simple yet effective method of time management that aims to improve work and study habits through structured "work" (or "pomodoro") and "break" segments. This technique was created by Francesco Cirillo and helps keep the user on task and focused during the "work" segments, while giving them adequate time for rest and recouperation during the "break" segments. This technique helps reduce the chances of burnout and can be an effective strategy towards increasing productivity. Learn more about the Pomodoro Technique on <a href="https://francescocirillo.com/pages/pomodoro-technique" target="_blank" rel="noopener noreferrer">Francesco Cirillo's website</a>.</p>
+  handleModalClose = () => {
+    this.setState({
+      modalOpen: false
+    });
+  };
 
-                                <p>This application, the TomatoTracker, provides the user with a timer that will accurately track the user's time spent during a "pomodoro" or break and log their completed time segments. This gives the user a sense of accomplishment and a graphical representation of their hard work! The user can also log comments to each of their time segments to see how they spent their time. This will aid when reviewing the work completed but also give the user a better ability to estimate the time needed to complete certain tasks in the future.</p>
+  render() {
+    return (
+      <div>
+        <Button
+          variant="contained"
+          color="default"
+          onClick={this.handleModalOpen}
+        >
+          More Info
+        </Button>
+        <Modal open={this.state.modalOpen} onClose={this.handleModalClose}>
+          <Fade in={this.state.modalOpen} timeout={{ enter: 500, exit: 500 }}>
+            <div className={'modal-window'}>
+              <div className={'modal-inner-wrapper'}>
+                <Fab className="modal-close" onClick={this.handleModalClose}>
+                  <AddIcon />
+                </Fab>
 
-                                <h3>The basics:</h3>
-                                <ol>
-                                    <li>Start the "Pomodoro" timer and work for 25 minutes. Make an effort to avoid distractions during this time.</li>
-                                    <li>Take a 5 minute break! You've earned it. Get up, stretch, meditate, grab a cup of coffee or even browse social media.</li>
-                                    <li>Rinse and repeat until you have completed 4 Pomodoros.</li>
-                                    <li>After completing 4 Pomodoros, take a 10 minute break. This longer break ensures that you are giving yourself enough time to rest and digest the information from your work or study sessions.</li>
-                                    <li>Continue this cycle as needed!</li>
-                                </ol>
+                <h2>Instructions and More Information</h2>
 
-                                <h3>This app was inspired by:</h3>
-                                <ol>
-                                    <li><a href="https://tomato-timer.com/" target="_blank" rel="noopener noreferrer">TomatoTimer</a></li>
-                                    <li><a href="http://tomatoi.st/" target="_blank" rel="noopener noreferrer">Tomatoist</a></li>
-                                </ol>
+                <p>
+                  The Pomodoro Technique is a simple yet effective method of
+                  time management that aims to improve work and study habits
+                  through structured "work" (or "pomodoro") and "break"
+                  segments. This technique was created by Francesco Cirillo and
+                  helps keep the user on task and focused during the "work"
+                  segments, while giving them adequate time for rest and
+                  recouperation during the "break" segments. This technique
+                  helps reduce the chances of burnout and can be an effective
+                  strategy towards increasing productivity. Learn more about the
+                  Pomodoro Technique on{' '}
+                  <a
+                    href="https://francescocirillo.com/pages/pomodoro-technique"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Francesco Cirillo's website
+                  </a>
+                  .
+                </p>
 
-                                <p>This application was built using the React Javascript framework so I could gain a deeper understanding of these technologies and to also provide myself with a useful tool for my future productivity. This application uses cookies to store the settings and Pomodoro data.</p>
+                <p>
+                  This application, the TomatoTracker, provides the user with a
+                  timer that will accurately track the user's time spent during
+                  a "pomodoro" or break and log their completed time segments.
+                  This gives the user a sense of accomplishment and a graphical
+                  representation of their hard work! The user can also log
+                  comments to each of their time segments to see how they spent
+                  their time. This will aid when reviewing the work completed
+                  but also give the user a better ability to estimate the time
+                  needed to complete certain tasks in the future.
+                </p>
 
-                                <h3>Why use this app?</h3>
+                <h3>The basics:</h3>
+                <ol>
+                  <li>
+                    Start the "Pomodoro" timer and work for 25 minutes. Make an
+                    effort to avoid distractions during this time.
+                  </li>
+                  <li>
+                    Take a 5 minute break! You've earned it. Get up, stretch,
+                    meditate, grab a cup of coffee or even browse social media.
+                  </li>
+                  <li>
+                    Rinse and repeat until you have completed 4 Pomodoros.
+                  </li>
+                  <li>
+                    After completing 4 Pomodoros, take a 10 minute break. This
+                    longer break ensures that you are giving yourself enough
+                    time to rest and digest the information from your work or
+                    study sessions.
+                  </li>
+                  <li>Continue this cycle as needed!</li>
+                </ol>
 
-                                <p>This application gives the user an easy to use Pomodoro Timer that also allows them to track their time spent working and create detailed logs of how they spent their time.</p>
-                                
-                                <h3>Features and settings include:</h3>
-                                <ol>
-                                    <li>Customize Pomodoro and Break times</li>
-                                    <li>Pick from a selection of alarm sounds</li>
-                                    <li>Adjust alarm volume</li>
-                                    <li>Toggle between showing only Pomodoros or showing Pomodoros and Breaks in the log</li>
-                                    <li>A "skip to the end of the timer" button. This is mainly used for demonstration purposes</li>
-                                </ol>
+                <h3>This app was inspired by:</h3>
+                <ol>
+                  <li>
+                    <a
+                      href="https://tomato-timer.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      TomatoTimer
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="http://tomatoi.st/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Tomatoist
+                    </a>
+                  </li>
+                </ol>
 
-                                <h3>Find me on the following platforms:</h3>
-                                <ul className={'external-links'}>
-                                    <li><a href="https://github.com/Matthew-Nelson" target="_blank" rel="noopener noreferrer"><FaGithub/>My Github</a></li>
-                                    <li><a href="https://www.linkedin.com/in/matthewbnelson/" target="_blank" rel="noopener noreferrer"><FaLinkedin/>My 
-                                    LinkedIn</a></li>
-                                    <li><a href="https://mattnelson.dev/" target="_blank" rel="noopener noreferrer"><FaLaptopCode/>My personal site</a></li>
-                                </ul>
-                                <br/>
-                            </div>
-                        </div>
-                    </Fade>
-                </Modal>
+                <p>
+                  This application was built using the React Javascript
+                  framework so I could gain a deeper understanding of these
+                  technologies and to also provide myself with a useful tool for
+                  my future productivity. This application uses cookies to store
+                  the settings and Pomodoro data.
+                </p>
+
+                <h3>Why use this app?</h3>
+
+                <p>
+                  This application gives the user an easy to use Pomodoro Timer
+                  that also allows them to track their time spent working and
+                  create detailed logs of how they spent their time.
+                </p>
+
+                <h3>Features and settings include:</h3>
+                <ol>
+                  <li>Customize Pomodoro and Break times</li>
+                  <li>Pick from a selection of alarm sounds</li>
+                  <li>Adjust alarm volume</li>
+                  <li>
+                    Toggle between showing only Pomodoros or showing Pomodoros
+                    and Breaks in the log
+                  </li>
+                  <li>
+                    A "skip to the end of the timer" button. This is mainly used
+                    for demonstration purposes
+                  </li>
+                </ol>
+
+                <h3>Find me on the following platforms:</h3>
+                <ul className={'external-links'}>
+                  <li>
+                    <a
+                      href="https://github.com/Matthew-Nelson"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaGithub />
+                      My Github
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/matthewbnelson/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLinkedin />
+                      My LinkedIn
+                    </a>
+                  </li>
+                  {/* <li><a href="https://mattnelson.dev/" target="_blank" rel="noopener noreferrer"><FaLaptopCode/>My personal site</a></li> */}
+                </ul>
+                <br />
+              </div>
             </div>
-        )
-    }
+          </Fade>
+        </Modal>
+      </div>
+    );
+  }
 }
--- src/Footer.js
+++ src/Footer.js
@@ -5,16 +5,32 @@ import { FaLinkedin } from 'react-icons/fa';
 import { FaLaptopCode } from 'react-icons/fa';
 
 export default function Footer() {
-    return (
-        <div className={"footer-container"}>
-            <Container className={"footer"} maxWidth="md">
-                <p>Created by Matthew Nelson</p>
-                <ul className={"external-links"}>
-                    <li><a href="https://github.com/Matthew-Nelson" target="_blank" rel="noopener noreferrer"><FaGithub/></a></li>
-                    <li><a href="https://www.linkedin.com/in/matthewbnelson/" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a></li>
-                    <li><a href="https://mattnelson.dev/" target="_blank" rel="noopener noreferrer"><FaLaptopCode/></a></li>
-                </ul>
-            </Container>
-        </div>
-    )
+  return (
+    <div className={'footer-container'}>
+      <Container className={'footer'} maxWidth="md">
+        <p>Created by Matthew Nelson</p>
+        <ul className={'external-links'}>
+          <li>
+            <a
+              href="https://github.com/Matthew-Nelson"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/matthewbnelson/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </li>
+          {/* <li><a href="https://mattnelson.dev/" target="_blank" rel="noopener noreferrer"><FaLaptopCode/></a></li> */}
+        </ul>
+      </Container>
+    </div>
+  );
 }
